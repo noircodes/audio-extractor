268e30c9d1cd8d5e5485572ff66918a1a26be417
noircodes <ilhamizdhihar2007@gmail.com>
2022-01-28 16:11:46 +0700

    Initialize and done the main code

---
+++ extractor.py
@@ -0,0 +1,9 @@
+import moviepy.editor
+
+video = moviepy.editor.VideoFileClip("test.mp4")
+
+# if your video is in same folder share your python file is there than you can give name direct
+# else you need to give path of the video file
+
+audio = video.audio
+audio.write_audioFile("extracted.mp3")
